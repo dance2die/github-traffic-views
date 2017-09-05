238f8812163d66c1fc1b62f3ea8a1661d6126abb
dance2die <dontbothermewithspam@gmail.com>
2017-09-05 18:42:17 -0400

    Displays a line chart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,136 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+import PropTypes from 'prop-types';
+import Visitor from './visitor';
+
+// debug
+ const l = console.log;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repos: props.repos,
+      visitorDetail: {},
+    }
+  }
+
+  componentDidMount() {
+    // const repoURL = `https://api.github.com/users/dance2die/repos?sort=updated&direction=desc&per_page=3000`;
+    // axios.get(repoURL)
+    //   .then(response => {
+    //     l(response);
+    //   })
+    //   .catch(error => {
+    //     alert("error!!!!", error);
+    //   });
+
+    let visitorDetail = {
+      "count": 14850,
+      "uniques": 3782,
+      "views": [
+        {
+          "timestamp": "2016-10-10T00:00:00Z",
+          "count": 440,
+          "uniques": 143
+        },
+        {
+          "timestamp": "2016-10-11T00:00:00Z",
+          "count": 1308,
+          "uniques": 414
+        },
+        {
+          "timestamp": "2016-10-12T00:00:00Z",
+          "count": 1486,
+          "uniques": 452
+        },
+        {
+          "timestamp": "2016-10-13T00:00:00Z",
+          "count": 1170,
+          "uniques": 401
+        },
+        {
+          "timestamp": "2016-10-14T00:00:00Z",
+          "count": 868,
+          "uniques": 266
+        },
+        {
+          "timestamp": "2016-10-15T00:00:00Z",
+          "count": 495,
+          "uniques": 157
+        },
+        {
+          "timestamp": "2016-10-16T00:00:00Z",
+          "count": 524,
+          "uniques": 175
+        },
+        {
+          "timestamp": "2016-10-17T00:00:00Z",
+          "count": 1263,
+          "uniques": 412
+        },
+        {
+          "timestamp": "2016-10-18T00:00:00Z",
+          "count": 1402,
+          "uniques": 417
+        },
+        {
+          "timestamp": "2016-10-19T00:00:00Z",
+          "count": 1394,
+          "uniques": 424
+        },
+        {
+          "timestamp": "2016-10-20T00:00:00Z",
+          "count": 1492,
+          "uniques": 448
+        },
+        {
+          "timestamp": "2016-10-21T00:00:00Z",
+          "count": 1153,
+          "uniques": 332
+        },
+        {
+          "timestamp": "2016-10-22T00:00:00Z",
+          "count": 566,
+          "uniques": 168
+        },
+        {
+          "timestamp": "2016-10-23T00:00:00Z",
+          "count": 675,
+          "uniques": 184
+        },
+        {
+          "timestamp": "2016-10-24T00:00:00Z",
+          "count": 614,
+          "uniques": 237
+        }
+      ]
+    };
+
+    this.setState({
+      ...this.state,
+      visitorDetail: visitorDetail
+    }, (value) => {
+      // l("app.value", this.state);
+    });
+  }
+
   render() {
+    const { repos, visitorDetail } = this.state;
+    // l("App", this.state);
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Visitor repo={repos[0]} visitorDetail={visitorDetail} />
       </div>
     );
   }
 }
+App.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
 
 export default App;
---
+++ src/visitor.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Chart } from 'react-google-charts';
+import './visitor.css';
+// import axios from 'axios';
+// import apiKeys from './apiKeys';
+
+const l = console.log;
+
+export default class Visitor extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            repo: props.repo,
+            visitorDetail: {},
+            chartConfig: {
+                "chartType": "LineChart",
+                "colors": ['#a52714', '#097138'],
+                "options": {
+                    "title": 'Date vs. Views',
+                    "hAxis": { "title": 'Date' },
+                    "vAxis": { "title": 'Views' },
+                },
+                "columns": [
+                    { "type": 'date', "label": 'Date' },
+                    { "type": 'number', "label": 'Count' },
+                    { "type": 'number', "label": 'Uniques' },
+                ]
+            },
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            ...this.state,
+            visitorDetail: nextProps.visitorDetail
+        });
+    }
+
+    render() {
+        const { repo, chartConfig, visitorDetail } = this.state;
+
+        if (!visitorDetail.views || visitorDetail.views.length === 0) {
+            return <div>loading chart...</div>;
+        }
+
+        const rows = visitorDetail.views.map(view => {
+            var row = Object.keys(view).map(key => {
+                if (key === "timestamp")
+                    return new Date(view[key]);
+                return view[key];
+            });
+            l("row", row);
+            return row;
+        });
+
+        l("returning chart...", this.state, rows);
+
+        const chart = <Chart
+            chartType={chartConfig.chartType}
+            rows={rows}
+            options={chartConfig.options}
+            columns={chartConfig.columns}
+            graph_id="LineChart"
+            width="100%"
+            height="150px"
+        />;
+
+        l("chart", chart);
+
+        return (
+            <div className="Visitor">
+                <header className="Visitor-header">{repo.full_name}</header>
+                <section className="Visitor-content">
+                    {chart}
+                </section>
+            </div>
+        );
+    }
+}
