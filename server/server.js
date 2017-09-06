1e74703da66d2825fec5c4650546cec26ce7b46c
dance2die <dontbothermewithspam@gmail.com>
2017-09-06 00:44:59 -0400

    Procfile Change

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const apiKeys = require('../src/apiKeys');
+const apiKeys = require('./apiKeys');
 const axios = require('axios');
 
 const l = console.log;
