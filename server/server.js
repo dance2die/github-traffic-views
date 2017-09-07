ef1bda7beb530feb344c4875f51cbabfefa842fd
dance2die <dontbothermewithspam@gmail.com>
2017-09-06 20:04:51 -0400

    1. Fixed CallBack Hell, 2. Fixed CSS issue

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
 // https://medium.com/@patriciolpezjuri/using-create-react-app-with-react-router-express-js-8fa658bf892d
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-function getAuth() {
+getAuth = () => {
     // l("apiKeys.GITHUB_DEVELOPER_KEY", apiKeys);
     const password = process.env.GITHUB_DEVELOPER_KEY;
     // l("password:", password);
@@ -34,83 +34,121 @@ function getAuth() {
     };
 }
 
-function getRepos(user) {
-    const url = `https://api.github.com/users/${user}/repos?sort=updated&direction=desc&per_page=10`;
-    return axios.get(url, { auth: getAuth() });
-};
+getVisitorDetail = (repo) => {
+    // l("-- getVisitorDetail --");
 
-getVisitorDetail = (user, repo) => {
-    const url = `https://api.github.com/repos/${user}/${repo}/traffic/views`;
+    const { login } = repo.owner;
+    const url = `https://api.github.com/repos/${login}/${repo.name}/traffic/views`;
     return axios.get(url, { auth: getAuth() });
 }
 
-getUserDetail = (user) => {
-    const url = `https://api.github.com/users/${user}`;
+getUserDetail = (login) => {
+    // l("-- getUserDetail --");
+
+    const url = `https://api.github.com/users/${login}`;
     return axios.get(url, { auth: getAuth() });
 }
 
-app.get('/getRepos/:user', (req, res) => {
-    let user = req.params.user || "dance2die";
-    getRepos(user)
-        .then(response => {
-            let repos = response.data;
-            l("repos returned:", repos);
-            // https://stackoverflow.com/questions/13554319/express-js-close-response
-            res.set("Connection", "close");
-            res.json(repos);
-        })
-        .catch(error => {
-            l("error!", error);
-        });
-});
+getRepos = (user) => {
+    const login = user.data.login;
+    const url = `https://api.github.com/users/${login}/repos?sort=updated&direction=desc&per_page=10`;
 
-app.get('/getVisitorDetail/:user/:repo', (req, res) => {
-    let user = req.params.user || "dance2die";
-    let repo = req.params.repo || "MyAnimeListSharp";
+    return axios.get(url, { auth: getAuth() });
+}
 
-    getVisitorDetail(user, repo)
-        .then(response => {
-            let details = response.data;
-            l("details returnd:", details);
-            // https://stackoverflow.com/questions/13554319/express-js-close-response
-            res.set("Connection", "close");
-            res.json(details);
-        })
-        .catch(error => {
-            l("error!", error);
-        });
-});
+getVisitorDetails = ({ data: repos }) => {
+    let promises = repos.map(repo => {
+        return getVisitorDetail(repo)
+            .then(response => {
+                let visitorDetail = response.data;
+                // l("visitorDetail!!!", visitorDetail);
+                return { key: repo.name, value: visitorDetail, repo: repo };
+            })
+            .catch(error => {
+                l("repo error", error);
+                // alert("error!", error);
+                return { key: repo.name, value: {} };
+            });
+    });
+
+    return Promise.all(promises);
+}
+
+
+// app.get('/getRepos/:user', (req, res) => {
+//     let user = req.params.user || "dance2die";
+//     getRepos(user)
+//         .then(response => {
+//             let repos = response.data;
+//             l("repos returned:", repos);
+//             // https://stackoverflow.com/questions/13554319/express-js-close-response
+//             res.set("Connection", "close");
+//             res.json(repos);
+//         })
+//         .catch(error => {
+//             l("error!", error);
+//         });
+// });
+
+// app.get('/getVisitorDetail/:user/:repo', (req, res) => {
+//     let user = req.params.user || "dance2die";
+//     let repo = req.params.repo || "MyAnimeListSharp";
+
+//     getVisitorDetail(user, repo)
+//         .then(response => {
+//             let details = response.data;
+//             l("details returnd:", details);
+//             // https://stackoverflow.com/questions/13554319/express-js-close-response
+//             res.set("Connection", "close");
+//             res.json(details);
+//         })
+//         .catch(error => {
+//             l("error!", error);
+//         });
+// });
 
 app.get('/visitorMap/:user', (req, res) => {
     let user = req.params.user || "dance2die";
     l("user:", user);
 
-    getRepos(user)
-        .then(response => {
-            let repos = response.data;
-            let visitorPromises = repos.map(repo => {
-                l("repo", repo);
-                return getVisitorDetail(user, repo.name)
-                    .then(response => {
-                        let visitorDetail = response.data;
-                        return { key: repo.name, value: visitorDetail, repo: repo };
-                    })
-                    .catch(error => {
-                        l("repo error", error);
-                        // alert("error!", error);
-                        return { key: repo.name, value: {} };
-                    });
-            });
+    getUserDetail("dance2die")
+    .then(getRepos)
+    .then(getVisitorDetails)
+    .then(visitorMap => {
+        // https://stackoverflow.com/questions/13554319/express-js-close-response
+        res.set("Connection", "close");
+        res.send(visitorMap);
+    })
+    .catch(error => {
+        l("error!", error);
+    });
+    
+    // getRepos(user)
+    //     .then(response => {
+    //         let repos = response.data;
+    //         let visitorPromises = repos.map(repo => {
+    //             l("repo", repo);
+    //             return getVisitorDetail(user, repo.name)
+    //                 .then(response => {
+    //                     let visitorDetail = response.data;
+    //                     return { key: repo.name, value: visitorDetail, repo: repo };
+    //                 })
+    //                 .catch(error => {
+    //                     l("repo error", error);
+    //                     // alert("error!", error);
+    //                     return { key: repo.name, value: {} };
+    //                 });
+    //         });
 
-            Promise.all(visitorPromises).then(visitorMap => {
-                // https://stackoverflow.com/questions/13554319/express-js-close-response
-                res.set("Connection", "close");
-                res.json(visitorMap);
-            });
-        })
-        .catch(error => {
-            l("Error while getting repos...", error);
-        });
+    //         Promise.all(visitorPromises).then(visitorMap => {
+    //             // https://stackoverflow.com/questions/13554319/express-js-close-response
+    //             res.set("Connection", "close");
+    //             res.json(visitorMap);
+    //         });
+    //     })
+    //     .catch(error => {
+    //         l("Error while getting repos...", error);
+    //     });
 });
 
 app.get('/', function (req, res) {
