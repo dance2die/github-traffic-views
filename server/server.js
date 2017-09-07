dca6e93773be4f95f9d83980f9d0331167700f50
dance2die <dontbothermewithspam@gmail.com>
2017-09-06 20:15:21 -0400

    Removed unused code

--- server/server.js
+++ server/server.js
@@ -8,18 +8,7 @@ const path = require('path');
 const l = console.log;
 
 // Enable CORS
-// // https://enable-cors.org/server_expressjs.html
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 app.use(cors());
-
-// const staticPath = path.join(__dirname, '../build');
-// // app.use(express.static('/public'));
-// // app.use(express.static('build/'));
-// app.use('/app', express.static(staticPath));
 
 // Serve static assets
 // https://medium.com/@patriciolpezjuri/using-create-react-app-with-react-router-express-js-8fa658bf892d
@@ -76,39 +65,6 @@ getVisitorDetails = ({ data: repos }) => {
     return Promise.all(promises);
 }
 
-
-// app.get('/getRepos/:user', (req, res) => {
-//     let user = req.params.user || "dance2die";
-//     getRepos(user)
-//         .then(response => {
-//             let repos = response.data;
-//             l("repos returned:", repos);
-//             // https://stackoverflow.com/questions/13554319/express-js-close-response
-//             res.set("Connection", "close");
-//             res.json(repos);
-//         })
-//         .catch(error => {
-//             l("error!", error);
-//         });
-// });
-
-// app.get('/getVisitorDetail/:user/:repo', (req, res) => {
-//     let user = req.params.user || "dance2die";
-//     let repo = req.params.repo || "MyAnimeListSharp";
-
-//     getVisitorDetail(user, repo)
-//         .then(response => {
-//             let details = response.data;
-//             l("details returnd:", details);
-//             // https://stackoverflow.com/questions/13554319/express-js-close-response
-//             res.set("Connection", "close");
-//             res.json(details);
-//         })
-//         .catch(error => {
-//             l("error!", error);
-//         });
-// });
-
 app.get('/visitorMap/:user', (req, res) => {
     let user = req.params.user || "dance2die";
     l("user:", user);
@@ -124,33 +80,6 @@ app.get('/visitorMap/:user', (req, res) => {
     .catch(error => {
         l("error!", error);
     });
-
-    // getRepos(user)
-    //     .then(response => {
-    //         let repos = response.data;
-    //         let visitorPromises = repos.map(repo => {
-    //             l("repo", repo);
-    //             return getVisitorDetail(user, repo.name)
-    //                 .then(response => {
-    //                     let visitorDetail = response.data;
-    //                     return { key: repo.name, value: visitorDetail, repo: repo };
-    //                 })
-    //                 .catch(error => {
-    //                     l("repo error", error);
-    //                     // alert("error!", error);
-    //                     return { key: repo.name, value: {} };
-    //                 });
-    //         });
-
-    //         Promise.all(visitorPromises).then(visitorMap => {
-    //             // https://stackoverflow.com/questions/13554319/express-js-close-response
-    //             res.set("Connection", "close");
-    //             res.json(visitorMap);
-    //         });
-    //     })
-    //     .catch(error => {
-    //         l("Error while getting repos...", error);
-    //     });
 });
 
 app.get('/', function (req, res) {
